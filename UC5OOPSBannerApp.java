8d6c066798f6334e75a364f76a85193fe3cdea44
nn2133-neeraj <nn2133@srmist.edu.in>
2026-02-24 09:19:57 +0530

    feature/UC5: oops

---
+++ UC5OOPSBannerApp.java
@@ -0,0 +1,28 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC5: Concise array declaration and initialization
+ * using String.join() within a single statement.
+ *
+ * @author Shukkur
+ * @version 5.0
+ */
+public class UC5OOPSBannerApp{
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+                String.join("", " *****   ", " *****   ", " *****   ", " ***** "),
+                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
+                String.join("", "*     * ", "*     * ", "*     * ", "*      "),
+                String.join("", "*     * ", "*     * ", " *****  ", " *****  "),
+                String.join("", "*     * ", "*     * ", "*       ", "     *  "),
+                String.join("", "*     * ", "*     * ", "*     * ", "*     * "),
+                String.join("", " *****   ", " *****   ", " *****   ", " ***** ")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
