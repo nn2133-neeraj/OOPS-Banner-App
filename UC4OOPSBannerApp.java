8d1fa4d6be82763214e3d63d8378cac7eb67ff61
nn2133-neeraj <nn2133@srmist.edu.in>
2026-02-24 08:55:46 +0530

    feature/UC:oops

---
+++ UC4OOPSBannerApp.java
@@ -0,0 +1,26 @@
+/ **
+* 00PSBannerApp UC4 - Render 00PS as Banner using String Array and Loop
+
+* This use case improves upon UC3 by using a String array to store banner lines
+* and iterating through them with a for-each loop, eliminating hardcoded print
+* statements and improving modularity and reusability.
+
+* @author Developer
+* @version 4
+*/
+
+// Extend the user story 3 to further develop the 00PS Banner Application
+// by using String arrays to hold banner lines and printing them in a loop.
+// This use case improves upon UC3 by using a String array to store banner lines
+// and iterating through them with a for-each loop, eliminating hardcoded print
+// statements and improving modularity and reusability.
+
+// Hint: .
+
+public class UC400PSBannerApp {
+
+// Main method to run the banner display
+Run | Debug
+public static void main(String[] args) {
+
+}
---
+++ UCOOPSBannerApp.java
@@ -0,0 +1,8 @@
+
+public class UCOOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("00PS");
+    }
+
+}
