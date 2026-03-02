fd5bb606981dd897cd84135f25cce9abda936381
nn2133-neeraj <nn2133@srmist.edu.in>
2026-03-02 08:46:12 +0530

    UC5: Render OOPS banner using inline array initialization

---
+++ UC6OOPSBannerApp.java
@@ -0,0 +1,58 @@
+public class UC6OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] bannerLines = {
+                String.join("", getOPattern(0), getOPattern(0), getPPattern(0), getSPattern(0)),
+                String.join("", getOPattern(1), getOPattern(1), getPPattern(1), getSPattern(1)),
+                String.join("", getOPattern(2), getOPattern(2), getPPattern(2), getSPattern(2)),
+                String.join("", getOPattern(3), getOPattern(3), getPPattern(3), getSPattern(3)),
+                String.join("", getOPattern(4), getOPattern(4), getPPattern(4), getSPattern(4)),
+                String.join("", getOPattern(5), getOPattern(5), getPPattern(5), getSPattern(5)),
+                String.join("", getOPattern(6), getOPattern(6), getPPattern(6), getSPattern(6))
+        };
+
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+
+    static String getOPattern(int row) {
+        String[] o = {
+                " *****  ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                " *****  "
+        };
+        return o[row];
+    }
+
+    static String getPPattern(int row) {
+        String[] p = {
+                " *****  ",
+                "*     * ",
+                "*     * ",
+                " *****  ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+        return p[row];
+    }
+
+    static String getSPattern(int row) {
+        String[] s = {
+                " *****  ",
+                "*      ",
+                "*      ",
+                " *****  ",
+                "      * ",
+                "      * ",
+                " *****  "
+        };
+        return s[row];
+    }
+}
